81b2f6370fcc1ce227d98d2cc642cf252ec60cef
Alexandru-Paul Copil <alexandru.p.copil@gmail.com>
2017-04-25 15:43:21 +0100

    Modify coalition.py main method for package testing

--- coalition/coalition.py
+++ coalition/coalition.py
@@ -1,11 +1,28 @@
 # -*- coding: utf-8 -*-
 """The main file of the coalition software package."""
-__version__ = '0.0.0dev4'
+
+import vcard
+import locale
+
+# VERSION USED BY PYPI
+__version__ = '0.0.0dev5'
 
 
 def main():
-    """The CLI."""
-    pass
+    """Main."""
+    print 'Main was ran'
+
+
+def p_vcard():
+    """Vcard."""
+    print 'Vcard'
+    print dir(vcard)
+
+
+def p_locale():
+    """Locale."""
+    print 'Locale'
+    print dir(locale)
 
 
 if __name__ == '__main__':
